c4dfcc90d46bdf1c588b73a57a338ccb08b4f2d4
Kobesani <Harold.Rockata@gmail.com>
2022-11-26 16:42:49 +0100

    Refactored items in separate module

--- scrape_projects/valorant/__init__.py
+++ scrape_projects/valorant/__init__.py
@@ -1,69 +1,14 @@
-import dataclasses
-import datetime
 import parsel
 import pendulum
 import requests
 import uplink
 
-from pendulum.tz.timezone import Timezone
-from typing import Optional, Tuple
+from typing import List
+from scrape_projects.valorant.items import ValorantResultItem, try_pendulum_timestamp
 
 BASE_URL = "https://vlr.gg"
+TINYBIRD_DATASOURCE_NAME = "valorant_results"
 
-
-def player_stats_process(value: Optional[str]) -> Optional[str]:
-    return True if value == "Player" else False
-
-
-def map_stats_process(value: Optional[str]) -> Optional[str]:
-    return True if value == "Map" else False
-
-
-def link_process(value: Optional[str]) -> Optional[str]:
-    return value
-
-
-def start_date_process(value: Optional[datetime.date]) -> Optional[datetime.date]:
-    return value
-
-
-def start_time_process(value: Optional[datetime.time]) -> Optional[datetime.time]:
-    return value
-
-
-def stakes_process(value: Optional[str]) -> Optional[str]:
-    return value
-
-
-def event_process(value: Optional[str]) -> Optional[str]:
-    return value
-
-
-def status_process(value: Optional[str]) -> Optional[str]:
-    return value
-
-
-def current_timezone_process(value: Optional[Timezone]) -> Optional[Timezone]:
-    return value
-
-
-@dataclasses.dataclass
-class ValorantResultItem:
-    link: Optional[str] = dataclasses.field(default=None)
-    start_date: Optional[datetime.date] = dataclasses.field(default=None)
-    start_time: Optional[datetime.time] = dataclasses.field(default=None)
-    player_stats: bool = dataclasses.field(default=False)
-    map_stats: bool = dataclasses.field(default=False)
-    stakes: Optional[str] = dataclasses.field(default=None)
-    status: Optional[str] = dataclasses.field(default=None)
-    event: Optional[str] = dataclasses.field(default=None)
-    current_timezone: Timezone = dataclasses.field(default=pendulum.now().tz)
-
-
-parent_queries = {
-    "cards": "//div[@class='wf-card']",
-    "matches": "./a[contains(@class, 'wf-module-item match-item')]",
-}
 
 leaf_queries = {
     "start_time": "./div[@class='match-item-time']",
@@ -76,7 +21,7 @@ leaf_queries = {
 }
 
 
-class ValorantResults(uplink.Consumer):
+class ValorantStatistics(uplink.Consumer):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(base_url=BASE_URL, *args, **kwargs)
 
@@ -84,63 +29,86 @@ class ValorantResults(uplink.Consumer):
     def get_results(self, page: uplink.Query("page", type=int)) -> requests.Response:
         pass
 
-    def scrape_results_page(self, page: int):
-        response = self.get_results(page=page)
+    @uplink.get("/{match_id}/{match_stub}")
+    def get_match(
+        self,
+        match_id: uplink.Path("match_id", type=int),
+        match_stub: uplink.Path("match_stub", type=str),
+    ) -> requests.Response:
+        pass
+
+
+class ValorantResults:
+    def __init__(self, consumer: uplink.Consumer = ValorantStatistics()) -> None:
+        self.consumer = consumer
+
+    def scrape_results_page(self, page: int) -> List[ValorantResultItem]:
+        response = self.consumer.get_results(page=page)
         main_selector = parsel.Selector(text=response.text)
+        cards = main_selector.xpath("//div[@class='wf-card']")
+        dates = (
+            main_selector.xpath("//div[@class='wf-label mod-large']")
+            .xpath("normalize-space(./text())")
+            .getall()
+        )
 
+        matches = []
+        for date, card in zip(dates, cards):
+            for match in card.xpath(
+                "./a[contains(@class, 'wf-module-item match-item')]"
+            ):
+                matches.append(
+                    ValorantResultItem(
+                        **(
+                            {"start_date": date, "link": match.xpath("@href").get()}
+                            | {
+                                leaf: match.xpath(query)
+                                .xpath("normalize-space(./text())")
+                                .get()
+                                for leaf, query in leaf_queries.items()
+                            }
+                        )
+                    )
+                )
 
-#     def scrape_results_in_page_range(self, start: int = 1, end: int = 1):
-#         results = []
-#         for page in range(start, end + 1):
-#             print(f"Scraping page {page}")
-#             results += self.scrape_single_results_page(page)
+        return matches
 
-#         return results
+    def get_matches_in_timeframe(self, timestamp_isoformat: str) -> List[ValorantResultItem]:
+        end_interval = pendulum.parse(timestamp_isoformat).in_tz(
+            pendulum.timezone("UTC")
+        )
+        start_interval = end_interval.subtract(days=1)
+        interval_started = False
+        interval_completed = False
+        next_page = 1
+        matches_in_range = []
 
-#     def scrape_single_results_page(self, page_number: int):
-#         data = self.scrape_endpoint("get_results_page", page_number)
-#         data["start_timestamp"] = [
-#             try_pendulum_timestamp(f"{d} {t}", "ddd, MMMM DD, YYYY hh:mm A")
-#             for d, t in zip(data["start_date"], data["start_time"])
-#         ]
-#         data["player_stats"] = [
-#             True if x == "Player" else False for x in data["player_stats"]
-#         ]
-#         data["map_stats"] = [True if x == "Map" else False for x in data["map_stats"]]
-#         data.pop("start_date", None)
-#         data.pop("start_time", None)
-#         return [
-#             {attribute: value for attribute, value in zip(data.keys(), row)}
-#             for row in zip(*data.values())
-#         ]
+        while not (interval_completed):
+            print(f"Starting parsing page={next_page}")
+            matches = self.scrape_results_page(next_page)
+            timestamps = [
+                pendulum.parse(
+                    try_pendulum_timestamp(
+                        f"{match.start_date} {match.start_time}",
+                        "ddd, MMMM DD, YYYY hh:mm A",
+                    )
+                )
+                for match in matches
+            ]
 
-#     def get_matches_in_timeframe(self, timestamp_isoformat: str) -> List[ResultData]:
-#         end_interval = pendulum.parse(timestamp_isoformat).in_tz(
-#             pendulum.timezone("UTC")
-#         )
-#         start_interval = end_interval.subtract(days=1)
-#         interval_started = False
-#         interval_completed = False
-#         next_page = 1
-#         matches_in_range = []
+            matches_in_range += [
+                match
+                for timestamp, match in zip(timestamps, matches)
+                if timestamp >= start_interval and timestamp < end_interval
+            ]
 
-#         while not (interval_completed):
-#             print(f"Starting parsing page={next_page}")
-#             matches = self.scrape_single_results_page(next_page)
+            if not (interval_started):
+                interval_started = len(matches_in_range) != 0
 
-#             matches_in_range += filter(
-#                 lambda x: x["start_timestamp"] >= start_interval
-#                 and x["start_timestamp"] < end_interval,
-#                 matches,
-#             )
+            interval_completed = interval_started and (
+                timestamps[-1] < start_interval
+            )
 
-#             if not (interval_started):
-#                 interval_started = len(matches_in_range) != 0
+            next_page += 1
 
-#             interval_completed = interval_started and (
-#                 matches[-1]["start_timestamp"] < start_interval
-#             )
-
-#             next_page += 1
-
-#         return matches_in_range
+        return matches_in_range
---
+++ scrape_projects/valorant/items.py
@@ -0,0 +1,82 @@
+import dataclasses
+import datetime
+import json
+import pendulum
+
+from pendulum.tz.timezone import Timezone
+from typing import Optional
+
+
+def player_stats_process(value: Optional[str]) -> Optional[str]:
+    return True if value == "Player" else False
+
+
+def map_stats_process(value: Optional[str]) -> Optional[str]:
+    return True if value == "Map" else False
+
+
+def link_process(value: Optional[str]) -> Optional[str]:
+    return value
+
+
+def start_date_process(value: Optional[datetime.date]) -> Optional[datetime.date]:
+    return value
+
+
+def start_time_process(value: Optional[datetime.time]) -> Optional[datetime.time]:
+    return value
+
+
+def stakes_process(value: Optional[str]) -> Optional[str]:
+    return value
+
+
+def event_process(value: Optional[str]) -> Optional[str]:
+    return value
+
+
+def status_process(value: Optional[str]) -> Optional[str]:
+    return value
+
+
+def current_timezone_process(value: Optional[Timezone]) -> Optional[Timezone]:
+    return value
+
+
+def try_pendulum_timestamp(timestamp: str, format: str):
+    try:
+        return (
+            pendulum.from_format(timestamp, format, tz=pendulum.now().tz)
+            .astimezone(pendulum.timezone("UTC"))
+            .isoformat()
+        )
+    except ValueError:
+        return None
+
+
+@dataclasses.dataclass
+class ValorantResultItem:
+    link: Optional[str] = dataclasses.field(default=None)
+    start_date: Optional[datetime.date] = dataclasses.field(default=None)
+    start_time: Optional[datetime.time] = dataclasses.field(default=None)
+    player_stats: bool = dataclasses.field(default=False)
+    map_stats: bool = dataclasses.field(default=False)
+    stakes: Optional[str] = dataclasses.field(default=None)
+    status: Optional[str] = dataclasses.field(default=None)
+    event: Optional[str] = dataclasses.field(default=None)
+    current_timezone: Timezone = dataclasses.field(default=pendulum.now().tz)
+
+    @property
+    def process_item(self) -> str:
+        data = dataclasses.asdict(self)
+        data["player_stats"] = player_stats_process(data.get("player_stats", None))
+        data["map_stats"] = map_stats_process(data.get("player_stats", None))
+        data["start_timestamp"] = try_pendulum_timestamp(
+            f"{data.get('start_date', None)} {data.get('start_time', None)}",
+            "ddd, MMMM DD, YYYY hh:mm A",
+        )
+        fields_to_pop = ["start_date", "start_time", "current_timezone"]
+        for field in fields_to_pop:
+            data.pop(field)
+        return json.dumps(data)
+
