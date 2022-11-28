c34c1c5e72675c02befd52e6dc7fed106460e540
Kobesani <Harold.Rockata@gmail.com>
2022-11-28 18:22:11 +0100

    Commenting out old datasource stuff

--- scrape_projects/valorant/datasources.py
+++ scrape_projects/valorant/datasources.py
@@ -61,40 +61,40 @@ VALORANT_RESULTS_DATASOURCE = Datasource(
     ],
 )
 
-DATASOURCES = {
-    "valorant_results": Datasource(
-        name="valorant_results",
-        format="ndjson",
-        mode="create",
-        schema=(
-            "event String `json:$.event`, "
-            "map_stats UInt8 `json:$.map_stats`, "
-            "player_stats UInt8 `json:$.player_stats`, "
-            "stakes String `json:$.stakes`, "
-            "status String `json:$.status`, "
-            "start_timestamp DateTime `json:$.start_timestamp`, "
-            "link String `json:$.link`"
-        ),
-    )
-}
-# results = tb.create_pipe("valorant_results_select_all", "select * from valorant_results_pages")
-PIPES = {
-    "valorant_results": Pipe(
-        datasource_name=DATASOURCES["valorant_results"].name,
-        name="valorant_results_select_all",
-        sql="select * from valorant_results",
-    )
-}
+# DATASOURCES = {
+#     "valorant_results": Datasource(
+#         name="valorant_results",
+#         format="ndjson",
+#         mode="create",
+#         schema=(
+#             "event String `json:$.event`, "
+#             "map_stats UInt8 `json:$.map_stats`, "
+#             "player_stats UInt8 `json:$.player_stats`, "
+#             "stakes String `json:$.stakes`, "
+#             "status String `json:$.status`, "
+#             "start_timestamp DateTime `json:$.start_timestamp`, "
+#             "link String `json:$.link`"
+#         ),
+#     )
+# }
+# # results = tb.create_pipe("valorant_results_select_all", "select * from valorant_results_pages")
+# PIPES = {
+#     "valorant_results": Pipe(
+#         datasource_name=DATASOURCES["valorant_results"].name,
+#         name="valorant_results_select_all",
+#         sql="select * from valorant_results",
+#     )
+# }
 
 
-NODES = {
-    "valorant_results": Node(
-        datasource_name=DATASOURCES["valorant_results"].name,
-        pipe_name=PIPES["valorant_results"].name,
-        name="matches_played_per_day",
-        sql=(
-            "select toDate(start_timestamp) match_date, count() matches_played "
-            "from valorant_results_select_all group by date"
-        ),
-    )
-}
+# NODES = {
+#     "valorant_results": Node(
+#         datasource_name=DATASOURCES["valorant_results"].name,
+#         pipe_name=PIPES["valorant_results"].name,
+#         name="matches_played_per_day",
+#         sql=(
+#             "select toDate(start_timestamp) match_date, count() matches_played "
+#             "from valorant_results_select_all group by date"
+#         ),
+#     )
+# }
