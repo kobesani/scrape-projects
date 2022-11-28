3ce32ea7a36a8d5f69a6f7a2b0b06ff2b7bafb3e
Kobesani <Harold.Rockata@gmail.com>
2022-11-28 17:16:05 +0100

    Adding new api consumer to get matches per day, pipe was created with nodes

--- scrape_projects/tinybird.py
+++ scrape_projects/tinybird.py
@@ -51,6 +51,56 @@ class DatasourcesApi(Consumer):
         pass
 
 
+class PipeApi(Consumer):
+    def __init__(self, token: str, *args, **kwargs) -> None:
+        super().__init__(
+            base_url="https://api.tinybird.co/",
+            auth=BearerToken(token=token),
+            *args,
+            **kwargs
+        )
+
+    @post("/v0/pipes")
+    def create_pipe(
+        self, name: Query("name", type=str), sql: Query(name="sql", type=str)
+    ):
+        pass
+
+    @get("/v0/sql")
+    def query_pipe(self, sql: Query(name="q", type=str)):
+        pass
+
+    @get("/v0/pipes")
+    def get_pipes(self):
+        pass
+
+    @returns.json
+    @get("/v0/pipes/{name}")
+    def get_pipe(self, name: Path(name="name", type=str)):
+        pass
+
+    @post("/v0/pipes/{pipe_name}/nodes")
+    def append_node(
+        self,
+        pipe_name: Path(name="pipe_name", type=str),
+        node_name: Query(name="name", type=str),
+        description: Query(name="description", type=str),
+        data: Body(type=str),
+    ):
+        pass
+
+    @put("/v0/pipes/{pipe}/endpoint")
+    def enable_node(self, pipe: uplink.Path(name="pipe", type=str), data: Body):
+        """
+        Enables a particular transformation for a pipe. This endpoint requires the
+        pipe name (param: pipe) and the id of the transformation node for the pipe.
+        By default, we enable the original pipe as the transformation node. Only one
+        node can be enabled at a time. You can query the pipe to get the ID of the node
+        you want to enable via get_pipe or get_pipes method.
+        """
+        pass
+
+
 class TinyBirdApi(Consumer):
     def __init__(self, token: str, *args, **kwargs) -> None:
         super().__init__(
@@ -105,3 +155,23 @@ class TinyBirdApi(Consumer):
     @put("/v0/pipes/{pipe}/endpoint")
     def enable_node(self, pipe: uplink.Path(name="pipe", type=str), data: Body):
         pass
+
+
+class ValorantDatasourceApi(Consumer):
+    def __init__(self, token: str, *args, **kwargs) -> None:
+        super().__init__(
+            base_url="https://api.tinybird.co/",
+            auth=BearerToken(token=token),
+            *args,
+            **kwargs
+        )
+
+    @returns.json
+    @get("/v0/pipes/valorant_results_select_all.json")
+    def query_matches_played(
+        self,
+        start_date: Query(name="start_date", type=str) = "2021-11-03 00:00:00",
+        end_date: Query("end_date", type=str) = "2021-11-04 00:00:00",
+    ):
+        pass
+
--- scrape_projects/valorant/datasources.py
+++ scrape_projects/valorant/datasources.py
@@ -1,5 +1,23 @@
 import dataclasses
 
+from typing import List, Optional
+
+
+@dataclasses.dataclass
+class Node:
+    datasource_name: str
+    pipe_name: str
+    name: str
+    sql: str
+
+
+@dataclasses.dataclass
+class Pipe:
+    datasource_name: str
+    name: str
+    sql: str
+    nodes: Optional[List[Node]]
+
 
 @dataclasses.dataclass
 class Datasource:
@@ -7,7 +25,41 @@ class Datasource:
     format: str
     mode: str
     schema: str
+    pipes: Optional[List[Pipe]]
 
+
+VALORANT_RESULTS_DATASOURCE = Datasource(
+    name="valorant_results",
+    format="ndjson",
+    mode="create",
+    schema=(
+        "event String `json:$.event`, "
+        "map_stats UInt8 `json:$.map_stats`, "
+        "player_stats UInt8 `json:$.player_stats`, "
+        "stakes String `json:$.stakes`, "
+        "status String `json:$.status`, "
+        "start_timestamp DateTime `json:$.start_timestamp`, "
+        "link String `json:$.link`"
+    ),
+    pipes=[
+        Pipe(
+            datasource_name="valorant_results",
+            name="valorant_results_select_all",
+            sql="select * from valorant_results",
+            nodes=[
+                Node(
+                    datasource_name="valorant_results",
+                    pipe_name="valorant_results_select_all",
+                    name="matches_played_per_day",
+                    sql=(
+                        "select toDate(start_timestamp) match_date, count() matches_played "
+                        "from valorant_results_select_all group by date"
+                    ),
+                )
+            ],
+        )
+    ],
+)
 
 DATASOURCES = {
     "valorant_results": Datasource(
@@ -22,6 +74,27 @@ DATASOURCES = {
             "status String `json:$.status`, "
             "start_timestamp DateTime `json:$.start_timestamp`, "
             "link String `json:$.link`"
+        ),
+    )
+}
+# results = tb.create_pipe("valorant_results_select_all", "select * from valorant_results_pages")
+PIPES = {
+    "valorant_results": Pipe(
+        datasource_name=DATASOURCES["valorant_results"].name,
+        name="valorant_results_select_all",
+        sql="select * from valorant_results",
+    )
+}
+
+
+NODES = {
+    "valorant_results": Node(
+        datasource_name=DATASOURCES["valorant_results"].name,
+        pipe_name=PIPES["valorant_results"].name,
+        name="matches_played_per_day",
+        sql=(
+            "select toDate(start_timestamp) match_date, count() matches_played "
+            "from valorant_results_select_all group by date"
         ),
     )
 }
