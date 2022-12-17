c281dfa4ac325f1080e201c82056414829b79d9e
Kobesani <Harold.Rockata@gmail.com>
2022-12-17 22:16:34 +0100

    Adding sql for getting matches from a particular day range, plus a script to scrape matches on daily basis

---
+++ scrape_projects/valorant/matches_in_range.sql
@@ -0,0 +1,4 @@
+%
+select * from valorant_results_select_all_0
+    where start_timestamp >= toDateTime({{DateTime(start_date, default='2021-11-03 00:00:00')}}) and
+          start_timestamp <= toDateTime({{DateTime(end_date, default='2021-11-05 00:00:00')}})
